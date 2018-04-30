9fa8617682802ff5bd234cc714ef396ea68d774b
da-matsumoto <da.mats1990@gmail.com>
2018-04-30 13:21:18 +0900

    スライダーに変更

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
-document.body.appendChild(canvas)
+document.getElementById('image').appendChild(canvas)
 
 const img = new Image()
 img.src = 'girl.jpg'
@@ -14,3 +14,17 @@ img.onload = () => {
 img.onerror = () => {
     console.log('画像の読み込み失敗')
 }
+
+const slider = document.getElementById('zoom-slider')
+slider.value = 1
+slider.min = 0.01
+slider.max = 2
+slider.step = 'any'
+
+slider.addEventListener('input', e => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    const scale = e.target.value
+    ctx.scale(scale,scale)
+    ctx.drawImage(img, 0, 0)
+    ctx.scale(1/ scale, 1/ scale)
+})
